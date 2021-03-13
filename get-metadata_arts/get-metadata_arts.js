3ba79bfee7c0c755b43bb20ab1ae6336e3cf22a6
ayberktandogan <ayberk.tandogan@outlook.com>
2021-03-13 13:04:39 +0300

    updates

--- get-metadata_arts/get-metadata_arts.js
+++ get-metadata_arts/get-metadata_arts.js
@@ -3,37 +3,47 @@
 // Bu dosyaları direkt olarak FFs node-server/image klasörüne atabilirsiniz.
 // NOT: Bulunamayan resimler de olacaktır. Bu dosyalar, hemen tespit edilsin diye silinmiyor,
 // 0 byte olarak kalıyor.
-require('dotenv').config()
-const CreateMetadataCanvas = require('./create_metadata_canvas')
-const { getPool } = require('./database')
+require("dotenv").config();
+const CreateMetadataCanvas = require("./create_metadata_canvas");
+const { getPool } = require("./database");
 
 async function getAnimeMetadataArts() {
-    const animes = await getPool(`SELECT * FROM anime`)
+    const animes = await getPool(`SELECT * FROM anime`);
     for await (const anime of animes) {
-        let { slug } = anime
+        let { slug } = anime;
 
-        console.log(slug)
+        console.log(`${slug}-anime için metadata görseli oluşturuluyor.`);
 
-        await CreateMetadataCanvas({ type: "anime", slug })
+        try {
+            await CreateMetadataCanvas({ type: "anime", slug });
+        } catch (err) {
+            console.log(`${slug}-anime için metadata görseli oluşturulamadı.`);
+        }
     }
 }
 
 async function getMangaMetadataArts() {
-    const mangas = await getPool(`SELECT * FROM manga`)
+    const mangas = await getPool(`SELECT * FROM manga`);
     for await (const manga of mangas) {
-        let { slug } = manga
+        let { slug } = manga;
 
-        console.log(slug)
+        console.log(`${slug}-manga için metadata görseli oluşturuluyor.`);
 
-        await CreateMetadataCanvas({ type: "manga", slug })
+        try {
+            await CreateMetadataCanvas({ type: "manga", slug });
+        } catch (err) {
+            console.log(`${slug}-manga için metadata görseli oluşturulamadı.`);
+        }
     }
 }
 
 async function getAllMetadataArts() {
-    await getAnimeMetadataArts()
-    await getMangaMetadataArts()
+    await getAnimeMetadataArts();
+    await getMangaMetadataArts();
+
+    console.log("\n\n\nİşlem tamamlandı.");
 }
 
-getAllMetadataArts()
+getAllMetadataArts();
 
-module.exports = { getPool }
+module.exports = { getPool };
